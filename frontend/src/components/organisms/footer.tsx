c38bade9f0fcd2bf4c2dc53cb8b7aed7c83d6ca4
mrredu <ejrb1234@gmail.com>
2025-01-16 18:59:24 -0400

    add: collection card + product card components

--- frontend/src/components/organisms/footer.tsx
+++ frontend/src/components/organisms/footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export const Footer = () => {
   return (
-    <footer className="bg-vampire min-h-[400px] px-2 py-5 md:grid-rows-2 md:px-4 lg:px-6">
+    <footer className="min-h-[400px] bg-vampire px-2 py-5 md:grid-rows-2 md:px-4 lg:px-6">
       <Image
         src="/logotype-negative.svg"
         alt="Negative logotype"
